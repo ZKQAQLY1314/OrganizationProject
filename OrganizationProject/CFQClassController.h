59ea43b4aeca03ac6aa74f5aa2d310ffd8d2161b
caofuqing <qianfeng@caofuqing.local>
2016-01-11 09:11:35 +0800

    diyicitijiao

---
+++ OrganizationProject/CFQClassController.h
@@ -0,0 +1,13 @@
+//
+//  CFQClassController.h
+//  OrganizationProject
+//
+//  Created by QianFeng on 16/1/11.
+//  Copyright © 2016年 曹福清. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CFQClassController : UIViewController
+
+@end
